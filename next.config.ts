058fb99596202e0c340beb1c28f4677d6ff8de85
Taran Mittal <taranmittal09@gmail.com>
2026-04-25 16:36:08 +1000

    please unbork

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const nextConfig = {
     unoptimized: true
   },
   basePath: '/horizons-crux',
-  assetPrefix: '/horizons-crux/public',
+  assetPrefix: '/horizons-crux/public/',
 };
 
 module.exports = nextConfig;
